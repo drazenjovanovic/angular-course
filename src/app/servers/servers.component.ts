c38a0b0d06952d40fb19ab6137dfa5456efac07b
jovanodraz <drazen.jovanovic@deltaholding.rs>
2019-10-14 21:26:15 +0200

    nastavak kursa

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServer = false;
-  constructor() { }
+  serverCreationStatus = 'No server was created';
+  serverName = 'testserver';
+  username = '';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
+  showSecret = false;
+  log = [];
+
+  constructor() {
+    setTimeout(() => { this.allowNewServer = true;}, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer(){
+
+    this.serverCreationStatus = 'Server was created. Name is ' + this.serverName;
+    this.servers.push(this.serverName);
+    this.serverCreated = true;
+
+  }
+
+  onToggleDetails(){
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1)
+    this.log.push(new Date());
+  }
+  onUpdateServerName(event:Event){
+
+    this.serverName = (<HTMLInputElement>event.target).value;
+
+  }
+
+  onReset(){
+    this.username = '';
   }
 
 }
